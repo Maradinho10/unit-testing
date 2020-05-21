68cd8e9240b58f036a81fbf09c7c950ef0565679
rbautista4 <reneeddermarin_bautista@apple.com>
2020-05-20 18:27:04 -0700

    H2 initialization added.

---
+++ src/test/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO contact(name, phone_number, comments, date_of_birth) VALUES('Andriy Shevchenko', '555-555-5555', 'ACM Player', '1976-09-29');
+INSERT INTO contact(name, phone_number, comments, date_of_birth) VALUES('Thierry Henry', '555-555-5555', 'Arsenal Player', '1977-08-17');
+INSERT INTO contact(name, phone_number, comments, date_of_birth) VALUES('Lionel Messi', '555-555-5555', 'FC Barcelona Player', '1987-06-24');
+
