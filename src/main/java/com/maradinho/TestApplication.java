17b0ce2c5eedef0745d30556b4b77195c97fafb7
rbautista4 <reneeddermarin_bautista@apple.com>
2020-05-30 18:53:04 -0700

    Swagger enabled.

--- src/main/java/com/maradinho/TestApplication.java
+++ src/main/java/com/maradinho/TestApplication.java
@@ -3,8 +3,10 @@ package com.maradinho;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
--- src/main/java/com/maradinho/config/ApplicationConfig.java
+++ src/main/java/com/maradinho/config/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package com.maradinho.application;
+package com.maradinho.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
